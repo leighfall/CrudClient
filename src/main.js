e0a95525349e996e7778fad40caad9f9d1c2615f
leighfall <autumn.leigh.henderson@gmail.com>
2022-07-07 16:57:34 -0400

    feat: Add style libraries

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
+import "materialize-css/dist/css/materialize.min.css";
+import "material-design-icons/iconfont/material-icons.css";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router).use(createPinia());
 
-app.mount('#app')
+app.mount("#app");
